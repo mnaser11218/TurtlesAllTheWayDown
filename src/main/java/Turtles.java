060b7d663390acffc489f176d07a0cbff7848474
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-08-02 12:10:51 -0400

    completed lab

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -4,4 +4,24 @@ package PACKAGE_NAME;
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+    public long factorial(int n) {
+        if (n == 1) return 1;
+        return n * factorial(n-1);
+    }
+
+    public int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+
+    // non-recursive implementation
+    public int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,69 @@
+import PACKAGE_NAME.Turtles;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+
+    @Test
+    public void recursionFactorialTest(){
+       Turtles turtles = new Turtles();
+       long actual = turtles.factorial(5);
+        long expected = 120;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void recursiongcdTest(){
+        Turtles turtles = new Turtles();
+        long actual = turtles.gcd(1440, 408);
+        long expected = 24;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void recursiongcd2Test(){
+        Turtles turtles = new Turtles();
+        long actual = turtles.gcd2(1440, 408);
+        long expected = 24;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void timingRecursionFactorial(){
+        Instant start = Instant.now();
+        Turtles turtles = new Turtles();
+        long actual = turtles.factorial(5);
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toNanos();
+        System.out.println(timeElapsed);
+    }
+
+    @Test
+    public void gcd2QuickerThangcdRecursion(){
+        Instant start = Instant.now();
+        Turtles turtles = new Turtles();
+        long actual = turtles.gcd(1440, 408);
+        Instant finish = Instant.now();
+        long timeElapsedforGCD = Duration.between(start, finish).toNanos();
+
+
+        Instant startgcd2 = Instant.now();
+        long actualgcd2 = turtles.gcd2(1440, 408);
+        Instant finishgcd2 = Instant.now();
+        long timeElapsedforGCD2 = Duration.between(startgcd2, finishgcd2).toNanos();
+
+        Assert.assertTrue(timeElapsedforGCD2 < timeElapsedforGCD);
+
+    }
+
+
+
 
 }
